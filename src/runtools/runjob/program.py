5dce7ba9f290c6c9ca455e37517cea6820a92621
stan svec <dev@stansvec.com>
2024-11-23 08:53:40 +0700

    program execution -> phase

--- src/runtools/runjob/program.py
+++ src/runtools/runjob/program.py
@@ -12,26 +12,24 @@ from typing import Union, Optional
 
 import sys
 
-from runtools.runcore.util.observer import CallableNotification
-from runtools.runjob.execution import OutputExecution, \
-    ExecutionException, ExecutionResult
+from runtools.runcore.run import FailedRun, TerminateRun, TerminationStatus
+from runtools.runjob.phaser import ExecutingPhase
 
 USE_SHELL = False  # For testing only
 
 log = logging.getLogger(__name__)
 
 
-class ProgramExecution(OutputExecution):
+class ProgramPhase(ExecutingPhase):
 
-    def __init__(self, *args, read_output: bool = True, tracking=None):
+    def __init__(self, phase_id, *args, read_output: bool = True):
+        super().__init__(phase_id)
         self.args = args
         self.read_output: bool = read_output
-        self._tracking = tracking
         self._popen: Union[Popen, None] = None
         self._status = None
         self._stopped: bool = False
         self._interrupted: bool = False
-        self._output_notification = CallableNotification()
 
     @property
     def ret_code(self) -> Optional[int]:
@@ -40,7 +38,7 @@ class ProgramExecution(OutputExecution):
 
         return self._popen.returncode
 
-    def execute(self):
+    def run(self, run_ctx):
         if not self._stopped and not self._interrupted:
             stdout = PIPE if self.read_output else None
             stderr = PIPE if self.read_output else None
@@ -50,46 +48,31 @@ class ProgramExecution(OutputExecution):
                 stdout_reader = None
                 stderr_reader = None
                 if self.read_output:
-                    stdout_reader = self._start_output_reader(self._popen.stdout, False)
-                    stderr_reader = self._start_output_reader(self._popen.stderr, True)
+                    stdout_reader = self._start_output_reader(run_ctx, self._popen.stdout, False)
+                    stderr_reader = self._start_output_reader(run_ctx, self._popen.stderr, True)
 
                 self._popen.wait()
                 if self.read_output:
                     stdout_reader.join(timeout=1)
                     stderr_reader.join(timeout=1)
                 if self.ret_code == 0:
-                    return ExecutionResult.DONE
+                    return
             except FileNotFoundError as e:
                 sys.stderr.write(str(e) + "\n")
                 """TODO Move exception level up"""
-                raise ExecutionException(str(e)) from e
+                raise FailedRun('CommandNotFound', str(e)) from e
 
         if self._interrupted or self.ret_code == -signal.SIGINT:
-            return ExecutionResult.INTERRUPTED
+            raise TerminateRun(TerminationStatus.INTERRUPTED)
         if self._stopped or self.ret_code < 0:  # Negative exit code means terminated by a signal
-            return ExecutionResult.STOPPED
-        raise ExecutionException("Process returned non-zero code " + str(self.ret_code))
+            raise TerminateRun(TerminationStatus.STOPPED)
+        raise FailedRun('ProgramError', "Process returned non-zero code " + str(self.ret_code))
 
-    def _start_output_reader(self, infile, is_err):
+    def _start_output_reader(self, run_ctx, infile, is_err):
         name = 'Stderr-Reader' if is_err else 'Stdout-Reader'
-        t = Thread(target=self._process_output, args=(infile, is_err), name=name, daemon=True)
+        t = Thread(target=self._process_output, args=(run_ctx, infile, is_err), name=name, daemon=True)
         t.start()
         return t
-
-    @property
-    def tracking(self):
-        return self._tracking
-
-    @tracking.setter
-    def tracking(self, tracking):
-        self._tracking = tracking
-
-    @property
-    def status(self):
-        if self.tracking:
-            return str(self.tracking)
-        else:
-            return self._status
 
     @property
     def parameters(self):
@@ -107,16 +90,10 @@ class ProgramExecution(OutputExecution):
         """
         self._interrupted = True
 
-    def add_callback_output(self, callback):
-        self._output_notification.add_observer(callback)
-
-    def remove_callback_output(self, callback):
-        self._output_notification.remove_observer(callback)
-
-    def _process_output(self, infile, is_err):
+    def _process_output(self, run_ctx, infile, is_err):
         with infile:
             for line in io.TextIOWrapper(infile, encoding="utf-8"):
                 line_stripped = line.rstrip()
                 self._status = line_stripped
                 print(line_stripped, file=sys.stderr if is_err else sys.stdout)
-                self._output_notification(line_stripped, is_err)
+                run_ctx.new_output(line_stripped, is_err)
