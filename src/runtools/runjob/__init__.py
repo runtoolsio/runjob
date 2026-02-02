d658b9fcd2b4206f62d776bc9ab746a2996a7fdc
stan <stan@taggstar.com>
2026-02-02 23:45:35 +0700

    moved clean stale sockets function to appropriate module

--- src/runtools/runjob/__init__.py
+++ src/runtools/runjob/__init__.py
@@ -6,53 +6,7 @@ IMPLEMENTATION NOTE:
     Avoid importing any module depending on any external package. This allows to use this module without installing
     additional packages.
 """
-from typing import List
-
-from runtools.runcore.util.socket import DatagramSocketClient
 from runtools.runjob import instance
 from runtools.runjob.instance import _JobInstance, JobInstanceHook
 
 __version__ = "0.11.0"
-
-_plugins = ()
-_persistence = ()
-
-
-def configure(**kwargs):
-    # TODO:
-    # max age and max records
-    # lock_timeout_sec = 10
-    # lock_max_check_time_sec = 0.05
-
-    plugins_obj = kwargs.get("plugins", {"enabled": False, "load": ()})
-    if plugins_obj.get("enabled", True):
-        global _plugins
-        _plugins = tuple(plugins_obj.get("load", ()))
-
-    persistence_array = kwargs.get("persistence", ())
-    dbs = []  # TODO max age and max records
-    for p in persistence_array:
-        if "type" not in p:
-            # raise InvalidConfiguration("Field `type` is mandatory in `persistence` configuration object")
-            pass
-        if not p.get("enabled", True):
-            continue
-        dbs.append(p)
-    global _persistence
-    _persistence = tuple(dbs)
-
-
-def clean_stale_sockets(file_extension) -> List[str]:
-    cleaned = []
-
-    c = DatagramSocketClient(file_extension)
-    try:
-        ping_result = c.ping()
-    finally:
-        c.close()
-
-    for stale_socket in ping_result.stale_sockets:
-        stale_socket.unlink(missing_ok=True)
-        cleaned.append(stale_socket.name)
-
-    return cleaned
